6b295d30d94aa8e535703eb5942e99fc8d2376cd
Aayush7015 <aayushjha.0111it221005@technocratsgroup.edu.in>
2026-02-13 11:38:20 +0530

    Updated Loginpagepom with latest changes

--- ECommerse/src/test/java/tech/testify/ECommerse/Loginpagepom.java
+++ ECommerse/src/test/java/tech/testify/ECommerse/Loginpagepom.java
@@ -1,5 +1,42 @@
 package tech.testify.ECommerse;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Loginpagepom {
 
+	
+WebDriver driver;
+	
+	@FindBy(xpath = "//input[@data-qa='login-email']")
+	WebElement logemail;
+	
+	@FindBy(xpath = "//input[@placeholder='Password']")
+	WebElement logpassword;
+	
+	@FindBy(xpath = "//button[normalize-space()='Login']")
+	WebElement logbtn;
+	
+	@FindBy(xpath = "//p[normalize-space()='Your email or password is incorrect!']")
+	WebElement incorrect;
+	
+	public Loginpagepom(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public  void loginpage(String mail, String pass) {
+		
+		logemail.sendKeys( mail);
+		logpassword.sendKeys(pass);
+		logbtn.click();
+		
+	}
+	
+	public boolean isincorrectmsgshown() {
+		return incorrect.isDisplayed();
+	}
+	
 }
